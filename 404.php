d86938d72d7b9e87117733baf47bee2d4740a564
designerjason <designerjason@users.noreply.github.com>
2014-08-11 16:36:37 +0100

    404 formatting

--- 404.php
+++ 404.php
@@ -10,19 +10,21 @@
  */
 ?>
 <?php Starkers_Utilities::get_template_parts( array( 'assets/includes/header' ) ); ?>
+
 <div class="container">
-<h1>Page not found</h1>
-<h2>This is somewhat embarrassing, isn't it?</h2>
+	<h1>Page not found</h1>
+	<h2>Well this is somewhat embarrassing, isn't it?</h2>
 
-<p>Sorry, but the page you were trying to view does not exist.</p>
+	<p>Sorry, but the page you were trying to view does not exist.</p>
 
-<p>It looks like this was the result of either:</p>
-<ul>
-    <li>a mistyped address</li>
-    <li>an out-of-date link</li>
-</ul>
+	<p>It looks like this was the result of either:</p>
+		
+		<ul>
+    		<li>a mistyped address</li>
+    		<li>an out-of-date link</li>
+		</ul>
 
-<p>If you think there should be a page here, please get in touch on <strong>xxxxxx</strong> , thanks!</p>
+	<p>If you think there should be a page here, please get in touch , thanks!</p>
 </div>
 
 <?php Starkers_Utilities::get_template_parts( array( 'assets/includes/footer' ) ); ?>
