4ad1e06fea592b0f4bc00f37cde4dbba48fb8895
designerjason <designerjason@users.noreply.github.com>
2014-08-13 15:49:37 +0100

    added antispambot shortcode

--- functions.php
+++ functions.php
@@ -52,6 +52,19 @@
 	}
 
 
+	// Hide email from Spam Bots using a shortcode.
+
+	function wpcodex_hide_email_shortcode( $atts , $content = null ) {
+		if ( ! is_email( $content ) ) {
+			return;
+		}
+
+		return '<a href="mailto:' . antispambot( $content ) . '">' . antispambot( $content ) . '</a>';
+	}
+	
+	add_shortcode( 'email', 'wpcodex_hide_email_shortcode' );
+
+
 	/* ========================================================================================================================
 	
 	Actions and Filters
