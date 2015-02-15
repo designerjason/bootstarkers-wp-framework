2b83d1edc7ebc175bab94773d74f383064e11646
designerjason <info@jmc-website-design.co.uk>
2015-02-15 12:19:25 +0000

    functions file amends

--- functions.php
+++ functions.php
@@ -16,28 +16,126 @@
 	======================================================================================================================== */
 
 	require_once( 'assets/includes/starkers-utilities.php' );
-	require_once( 'assets/includes/admin.php' );
 	//require_once( 'assets/includes/Mobile_Detect.php' );
 	//require_once( 'assets/includes/custom-post-type.php' );
 
 	/*======================================================================================================================== */
 
-	// Premium theme
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function startertheme_setup() {
+
+	/*
+	 * Make theme available for translation.
+	 * Translations can be filed in the /languages/ directory.
+	 * If you're building a theme based on startertheme, use a find and replace
+	 * to change 'startertheme' to the name of your theme in all the template files
+	 */
+	load_theme_textdomain( 'startertheme', get_template_directory() . '/languages' );
+
+	/*
+	 * Enable support for Post Thumbnails on posts and pages.
+	 *
+	 * @link http://codex.wordpress.org/Function_Reference/add_theme_support#Post_Thumbnails
+	 */
+	
 	add_theme_support( 'post-thumbnails' );
-	//add_theme_support( 'menus' );
-	
+	add_theme_support( 'menus' );
 
-	/** NAVIGATION **/
-	if ( function_exists( 'register_nav_menus' ) ) {
-		
-		register_nav_menus(
-			array(
-		  		'primary' => 'Primary Nav'
-			)
-		); 
-	
-	} 
+	// This theme uses wp_nav_menu() in one location.
+	register_nav_menus(
+		array(
+	  		'primary' => 'Primary Nav'
+		)
+	); 
+
+	/*
+	 * Switch default core markup for search form, comment form, and comments
+	 * to output valid HTML5.
+	 */
+	add_theme_support( 'html5', array(
+		'search-form', 'comment-form', 'comment-list', 'gallery', 'caption',
+	) );
+
+	/*
+	 * Enable support for Post Formats.
+	 * See http://codex.wordpress.org/Post_Formats
+	 */
+	//add_theme_support( 'post-formats', array(
+	//	'aside', 'image', 'video', 'quote', 'link',
+	//) );
+
+	// Set up the WordPress core custom background feature.
+	add_theme_support( 'custom-background', apply_filters( 'startertheme_custom_background_args', array(
+		'default-color' => 'ffffff',
+		'default-image' => '',
+	) ) );
+}
+
+
+
+
+
+
+
+
+
+
+/**
+ * Register widget area.
+ *
+ * @link http://codex.wordpress.org/Function_Reference/register_sidebar
+ */
+function startertheme_widgets_init() {
+	register_sidebar( array(
+		'name'          => __( 'Sidebar', 'startertheme' ),
+		'id'            => 'sidebar-1',
+		'description'   => '',
+		'before_widget' => '<aside id="%1$s" class="widget %2$s">',
+		'after_widget'  => '</aside>',
+		'before_title'  => '<h1 class="widget-title">',
+		'after_title'   => '</h1>',
+	) );
+}
+add_action( 'widgets_init', 'startertheme_widgets_init' );
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 	// Remove thumbnail width and height dimensions that prevent fluid images in the_thumbnail
@@ -108,15 +206,6 @@
 	// add_image_size( "thumb-mobile", width, height, True );
 
 
-	if( false === get_option("medium_crop") ) {
-	    
-	    add_option( "medium_crop", "1" );
-		
-		} else {
-	    	update_option( "medium_crop", "1" );
-	}
-
-
 	function new_excerpt_length( $length ) {
 		return 20;
 	}
@@ -174,38 +263,6 @@
 	add_filter( 'wp_head', 'IE_conditionals' );
 
 
-	/* ========================================================================================================================
-	
-	Comments
-	
-	======================================================================================================================== */
-
-	/**
-	 * Custom callback for outputting comments 
-	 *
-	 * @return void
-	 * @author Keir Whitaker
-	 */
-
-	function starkers_comment( $comment, $args, $depth ) {
-		$GLOBALS['comment'] = $comment; 
-		?>
-		<?php if ( $comment->comment_approved == '1' ): ?>	
-		<li>
-			<article id="comment-<?php comment_ID() ?>">
-				<?php echo get_avatar( $comment ); ?>
-				<h4><?php comment_author_link() ?></h4>
-				<time><a href="#comment-<?php comment_ID() ?>" pubdate><?php comment_date() ?> at <?php comment_time() ?></a></time>
-				<?php comment_text() ?>
-			</article>
-		<?php endif;
-	}
-
-
-	// No more jumping for read more link
-	function no_more_jumping( $post ) {
-		return '<a href="'.get_permalink($post->ID).'" class="read-more">'.'Continue Reading'.'</a>';
-	}
 	
 	add_filter( 'excerpt_more', 'no_more_jumping' );
 	add_filter( 'the_content_more_link', 'remove_more_jump_link' );
@@ -221,5 +278,17 @@
 	remove_action( 'wp_head', 'start_post_rel_link', 10, 0 );
 	remove_action( 'wp_head', 'parent_post_rel_link', 10, 0 );
 	remove_action( 'wp_head', 'adjacent_posts_rel_link', 10, 0 );
+
+
+/**
+ * Implement the Custom Header feature.
+ */
+//require get_template_directory() . '/assets/includes/custom-header.php';
+
+
+/**
+ * Custom template tags for this theme.
+ */
+require get_template_directory() . '/assets/includes/template-tags.php';
 
 	?>
